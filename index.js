d6bb55ea07ea096f68295fa96943de5fed5513a2
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-02-20 18:08:38 -0500

    Mongoose Part 2

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'is it pizza? or what?'
+      Dishes.create({
+        name: 'Uthapizza',
+        description: 'is it pizza ? or what ?'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 
 });
